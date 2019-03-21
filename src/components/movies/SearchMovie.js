10316d9097c75f979b712cb316d240146063ef8e
istvanzsolnai <zsolnai.istvan1995@gmail.com>
2019-03-21 13:01:41 +0100

    Search bar get JSON

--- src/components/movies/SearchMovie.js
+++ src/components/movies/SearchMovie.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class SearchMovie extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            searchText: '',
+            data: ""
+        }
+    }
+
+    handleOnChange(event){
+        this.setState({data: event.target.value});
+
+    }
+
+
+    handleSubmit(event){
+        event.preventDefault();
+        axios.get(`http://localhost:8888/movie/search?title=${this.state.data}`)
+            .then(res => {
+                const search = res.data;
+                this.setState(search);
+            })
+    }
 
     render() {
         return (
-            <form action="" style={{ display: 'flex' }}>
+            <form onSubmit={this.handleSubmit} style={{ display: 'flex' }}>
                 <input
+                    value={this.state.data} onChange={event => this.handleOnChange(event)}
                     type="text"
                     name="title"
                     style={{ flex: '10', padding: '10px' }}
