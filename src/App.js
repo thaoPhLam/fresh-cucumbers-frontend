f3b014e370c1fa5e8c68824fb506d117c176f5d6
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-03-21 14:16:36 +0100

    logs out the search result

--- src/App.js
+++ src/App.js
@@ -12,42 +12,27 @@ class App extends Component {
     movies: []
   };
 
-  handleSearch = (e) => {
-    let currentList = [];
-    // Variable to hold the filtered list before putting into state
-    let newList = [];
-
-    // If the search bar isn't empty
-    if (e.target.value !== "") {
-      // Assign the original list to currentList
-      currentList = this.props.movies;
-
-      // Use .filter() to determine which items should be displayed
-      // based on the search terms
-      newList = currentList.filter(item => {
-        // change current item to lowercase
-        const lc = item.toLowerCase();
-        // change search term to lowercase
-        const filter = e.target.value.toLowerCase();
-        // check to see if the current list item includes the search term
-        // If it does, it will be added to newList. Using lowercase eliminates
-        // issues with capitalization in search terms and search content
-        return lc.includes(filter);
-      });
-    } else {
-      // If the search bar is empty, set newList to original task list
-      newList = this.props.movies;
-    }
-    // Set the filtered state based on what our rules added to newList
-    this.setState({
-      filtered: newList
-    });
-  };
-
   componentDidMount() {
     axios.get('http://localhost:8888/movie/index')
         .then(res => this.setState({movies: res.data}));
   }
+
+  searchMovie = (movieTitle) => {
+
+    axios.get(`http://localhost:8888/movie/search?title=${movieTitle}`)
+    .then(res => this.setState({movies: res.data}))
+    //.then(res => console.log(res.data))
+    /* */
+    /*
+    fetch(`http://localhost:8888/movie/search?title=${movieTitle}`)
+        .then(resp => function () {
+          console.log(resp);
+        });
+
+    console.log("this is the state");
+    console.log(this.state);
+     */
+  };
 
   render() {
     return (
@@ -57,9 +42,9 @@ class App extends Component {
 
             <Header />
 
-            <Route exact path="/" render={movies => (
+            <Route exact path="/" render={() => (
               <React.Fragment>
-                  <SearchMovie searchMovie={this.handleSearch}/>
+                  <SearchMovie searchMovie={this.searchMovie}/>
                   <Movies movies={this.state.movies} />
               </React.Fragment>
             )} />
