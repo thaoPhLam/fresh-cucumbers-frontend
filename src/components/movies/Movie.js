43e377764903588f144fb08fd16891a5f61fcaf0
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-04-29 17:57:49 +0200

    restructured & styled Movie component

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,45 +1,26 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
+import './Movie.css';
 
 class Movie extends Component {
-    getStyle = () => {
-        return {
-            color: 'white',
-            textDecorationLine: 2,
-            display: 'block',
-            background: "#71121f",
-            padding: '10px',
-            borderBottom: '1px #ccc dotted',
-
-        }
-    };
-
-
 
     render() {
         const {Title,Poster,imdbRating,Plot,dbID} = this.props.movie;
 
         return (
-            <div style={this.getStyle()}>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <Link>
-                                <img src={Poster} alt=""/>
-                            </Link>
-                        </td>
-                        <td>
-                            <Link to="/movieDetails">{Title}</Link>
-                            <p>Rating: {imdbRating}</p>
-                            <p>{Plot}</p>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+            <article className="Movie">
+                <h4>
+                    <Link to="/movieDetails">{Title}</Link>
+                    <p>Rating: {imdbRating}</p>
+                </h4>
+                <div>
+                    <Link>
+                        <img src={Poster} alt=""/>
+                    </Link>
+                    <p>{Plot}</p>
+                </div>
+            </article>
         );
     }
 }
